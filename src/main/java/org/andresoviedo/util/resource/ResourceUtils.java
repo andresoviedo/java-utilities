275c8c38696c467780fbd692dbf91525792d98ce
andresoviedo <andresoviedo@gmail.com>
2013-11-13 10:28:47 +0100

    * ResourceUtils.class to read resources to string * Removed JNotify dependency

---
+++ src/main/java/org/andresoviedo/util/resource/ResourceUtils.java
@@ -0,0 +1,25 @@
+package org.andresoviedo.util.resource;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+import org.apache.commons.io.IOUtils;
+
+public class ResourceUtils {
+
+	public static final String getResourceAsString(String path)
+			throws IOException {
+		InputStream resourceAsStream = ResourceUtils.class
+				.getResourceAsStream(path);
+		if (resourceAsStream == null) {
+			return null;
+		}
+		List<String> lines = IOUtils.readLines(resourceAsStream, "UTF-8");
+		StringBuilder sb = new StringBuilder(lines.remove(0));
+		for (String line : lines) {
+			sb.append('\n').append(line);
+		}
+		return sb.toString();
+	}
+}
