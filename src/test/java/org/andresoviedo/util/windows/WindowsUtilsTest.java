dd9162934aea7f16c3a2c878b8eacd06bf09cde8
andresoviedo <andresoviedo@gmail.com>
2015-07-31 17:16:33 +0200

    Fixed compilation in Linux

--- src/test/java/org/andresoviedo/util/windows/WindowsUtilsTest.java
+++ src/test/java/org/andresoviedo/util/windows/WindowsUtilsTest.java
@@ -1,11 +1,19 @@
 package org.andresoviedo.util.windows;
 
+import org.andresoviedo.util.os.OSUtils;
+import org.apache.log4j.Logger;
 import org.junit.Test;
 
 public class WindowsUtilsTest {
 
+	private Logger LOG = Logger.getLogger(WindowsUtilsTest.class);
+
 	@Test
 	public void testInstallStartMenuItem() throws Exception {
+		if (!OSUtils.isWindows()) {
+			LOG.warn("Not in Widnows. Ignoring test...");
+			return;
+		}
 		WindowsUtils.installStartMenuItem(WindowsUtils.SPECIALFOLDER_Programs,
 				"my_start_menu", "explorer.exe", "http://www.google.es",
 				"Acceso director a google");
@@ -16,6 +24,10 @@ public class WindowsUtilsTest {
 
 	@Test
 	public void testInstallStartMenuItemForAllUsers() throws Exception {
+		if (!OSUtils.isWindows()) {
+			LOG.warn("Not in Widnows. Ignoring test...");
+			return;
+		}
 		WindowsUtils.installStartMenuItem(
 				WindowsUtils.SPECIALFOLDER_AllUsersPrograms, "my_start_menu",
 				"explorer.exe", "http://www.google.es",
